56125a1a9a7077ed6ed3f69c7d11bbf0d8d5a2d6
Daniel van Hoesel <daniel@danielvanhoesel.nl>
2017-09-28 13:51:35 +0200

    Make sure the plugin is compatible with React 16
    Had to copy an important part of React internals because it's not exposed in their packages.

--- demo/src/application.js
+++ demo/src/application.js
@@ -3,8 +3,8 @@ var ReactDOM = require("react-dom");
 var injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
 
-var Main = React.createClass({
-  render: function() {
+class Main extends React.Component {
+  render() {
     return React.createElement(
       "a", {
         href: "#",
@@ -13,15 +13,15 @@ var Main = React.createClass({
       },
       "Tap Me"
     );
-  },
+  }
 
-  handleClick: function(e) {
+  handleClick(e) {
     console.log("click", e);
-  },
+  }
 
-  handleTouchTap: function(e) {
+  handleTouchTap(e) {
     console.log("touchTap", e);
   }
-});
+}
 
 ReactDOM.render(React.createElement(Main), document.getElementById("container"));
